b3b2d0e16f5aabd9ac5aaf572897ca2369ad9d53
pennacap <64080101+pennacap@users.noreply.github.com>
2022-06-23 15:35:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,8 +9,31 @@ bot.key = os.getenv("API_KEY")
 bot.gname = os.getenv("GUILD_NAME")
 bot.log_channel = int(os.getenv("LOG_CHANNEL"))
 bot.role = int(os.getenv("GUILD_ROLE"))
+category = None
+lastUpdated = 0
+@tasks.loop(minutes=5)
+async def loop2():
+    global lastUpdated
+    a = requests.get("https://api.hypixel.net/resources/skyblock/election").json()
+    if lastUpdated == a["lastUpdated"]:
+        return
+    lastUpdated = a["lastUpdated"]
+    channels = category.channels
+    if channels[-1].name != "Current: "+a["mayor"]["name"]:
+        await channels[-1].edit(name="Current: "+a["mayor"]["name"])
+    if not "current" in a.keys():
+        for channel in channels[:5]:
+            if channel.name != "No Election On":
+                await channel.edit(name="No Election On")
+        return
+    for channel, mayor in zip(channels,a["current"]["candidates"]):
+        if channel.name != mayor["name"] + " - " + str(mayor["votes"]):
+            await channel.edit(name=mayor["name"] + " - " + str(mayor["votes"]))
+        
+        
+    
 @tasks.loop(hours=12)
-async def loop():
+async def loop1():
     role = bot.guilds[0].get_role(bot.role)
     a = requests.get(f"https://api.hypixel.net/guild?name={bot.gname.replace(' ','%20')}&key=0dcc1cec-f88c-44f2-877b-b173cb78bac0").json()
     file = []
@@ -49,7 +72,11 @@ async def loop():
 @bot.event
 async def on_ready():
     try:
-        loop.start()
+        loop1.start()
+    except Exception:
+        pass
+    try:
+        loop2.start()
     except Exception:
         pass
     
